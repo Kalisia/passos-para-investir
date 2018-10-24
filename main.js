c473444e07e7aca55c5e0ab9c6d3d101106f2d79
Kalisia Andrade <kalisia.autuori@gmail.com>
2018-10-24 14:13:52 -0300

    Criando projeto.

---
+++ main.js
@@ -0,0 +1,25 @@
+function investir(passo) {
+    ("Escolher uma boa corretora de valores");
+    
+    ("Abrir uma conta de investimento");
+    
+    ("Transferir o dinheiro");
+    
+    ("Definir seus objetivos");
+    
+    ("Traçar seu plano de investimento");
+    
+    ("Escolher as ações");
+    
+    ("Acessar o Home Broker");
+    
+    ("Enviar sua ordem de compra");
+    
+    ("Acompanhar a valorização");
+    
+    ("Realizar os lucros");
+    
+    return passo;
+}
+
+investir(passo);
