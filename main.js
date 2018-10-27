4c7417799b97d02f0bdbb68e840495a4ee470607
Kalisia Andrade <kalisia.autuori@gmail.com>
2018-10-26 23:35:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,3 @@
-function investir(passo) {
-    ("Escolher uma boa corretora de valores");
-    
-    ("Abrir uma conta de investimento");
-    
-    ("Transferir o dinheiro");
-    
-    ("Definir seus objetivos");
-    
-    ("Traçar seu plano de investimento");
-    
-    ("Escolher as ações");
-    
-    ("Acessar o Home Broker");
-    
-    ("Enviar sua ordem de compra");
-    
-    ("Acompanhar a valorização");
-    
-    ("Realizar os lucros");
-    
-    return passo;
-}
-
-//investir(passo);
-
 var idList = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
 
 function hide_all(idSkip) {
